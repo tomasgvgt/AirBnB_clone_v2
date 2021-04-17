67a2fc41e7cec69d42cfdd79c3bc0a0201cf81ec
tomasgvgt <tomasgvg.t@gmail.com>
2021-04-17 16:36:33 -0500

    101

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
 # Install nginx if not already installed
-exec { 'configuration':
-    command  => 'sudo apt-get -y update;
-                command -v nginx || sudo apt-get -y install nginx;
-                mkdir -p /data/web_static/shared;
+package {'nginx':
+    ensure => 'present'
+}
+exec { 'setup':
+    command  => 'mkdir -p /data/web_static/shared;
                 mkdir -p /data/web_static/releases/test;
                 echo "Testing Nginx configuration" > /data/web_static/releases/test/index.html;
                 ln -sf /data/web_static/releases/test /data/web_static/current;
