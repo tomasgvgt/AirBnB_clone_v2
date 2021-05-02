d006b478fd65762cd03c437f0c8bcd31bea13e0e
tomasgvgt <tomasgvg.t@gmail.com>
2021-05-02 14:16:57 -0500

    function definition

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -34,7 +34,7 @@ def states_list():
 
 @app.teardown_appcontext
 def teardown_appcontext(error):
-        storage.close()
+    storage.close()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
